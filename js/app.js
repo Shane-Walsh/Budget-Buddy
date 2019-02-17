c07a6720917fe09d9a10e3f3d6bdfd25ab125fda
Shane Walsh <walsh.shane@icloud.com>
2019-02-17 19:18:28 +0000

    Create controllers

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+var budgetController = (function(){
+
+   // do something
+
+})();
+
+
+
+var UIController = (function(){
+
+    // do something
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl){
+
+    // do something
+
+})(budgetController, UIController);
