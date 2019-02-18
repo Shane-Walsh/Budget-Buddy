60a52ac091ae1c98c530a0ad7bf9b67e9ac9b6ca
Shane Walsh <walsh.shane@icloud.com>
2019-02-18 14:26:17 +0000

    Create add item event listener

--- js/app.js
+++ js/app.js
@@ -6,7 +6,6 @@ var budgetController = (function(){
 })();
 
 
-
 var UIController = (function(){
 
     // do something
@@ -15,6 +14,31 @@ var UIController = (function(){
 
 var controller = (function(budgetCtrl, UICtrl){
 
-    // do something
+
+    var ctrlAddItem = function(){
+
+        // 1. Get field input data
+
+        // 2. Add item to budget controller
+
+        // 3. Add item to UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+
+    // Listen for return key press
+    document.addEventListener('keypress', function(event){
+
+        if(event.keyCode === 13 || event.which === 13){
+
+           ctrlAddItem();
+        }
+
+    });
 
 })(budgetController, UIController);
