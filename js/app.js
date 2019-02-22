370281b4202c1294e1ae0f05e66c39496b10a0be
Shane Walsh <walsh.shane@icloud.com>
2019-02-22 15:28:47 +0000

    Input field validation

--- js/app.js
+++ js/app.js
@@ -92,7 +92,7 @@ var UIController = (function(){
             return {
                     type: document.querySelector(DOMstrings.inputType).value, // inc or exp
                     description: document.querySelector(DOMstrings.inputDesc).value,
-                    value: document.querySelector(DOMstrings.inputValue).value
+                    value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
            };
         },
 
@@ -169,6 +169,15 @@ var controller = (function(budgetCtrl, UICtrl){
         });
     };
 
+    var updateBudget = function(){
+
+        // 1. Calculate the budget
+
+        // 2. Return budget
+
+        // 3. Display the budget on the UI
+    };
+
     var ctrlAddItem = function(){
 
         var input, newItem;
@@ -176,25 +185,28 @@ var controller = (function(budgetCtrl, UICtrl){
         // 1. Get field input data
         input = UICtrl.getInput();
 
-        // 2. Add item to budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        if(input.description !== "" && !isNaN(input.value) && input.value > 0){
 
-        // 3. Add item to UI
-        UICtrl.addListItem(newItem, input.type);
+            // 2. Add item to budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-        // 4. Clear input fields UI
-        UICtrl.clearFields();
+            // 3. Add item to UI
+            UICtrl.addListItem(newItem, input.type);
 
-        // 5. Calculate the budget
+            // 4. Clear input fields UI
+            UICtrl.clearFields();
 
-        // 6. Display the budget on the UI
+            // 5. Calc and update budget
+
+            updateBudget();
+        }
     };
 
     return {
 
         init: function () {
 
-            console.log('Application starting...');
+            console.log('Application running...');
             setupEventListeners();
         }
     }
