b0a4f2a5581d521c56396a7c54b11cead0fb1b47
Shane Walsh <walsh.shane@icloud.com>
2019-03-01 00:04:21 +0000

    Added change event listeners for UI styling

--- js/app.js
+++ js/app.js
@@ -212,6 +212,14 @@ var UIController = (function(){
         return (type === 'exp' ? sign = '-' : sign = '+') + ' ' + int + '.' + dec;
     };
 
+    var nodeListForEach = function(list, callback){
+
+        for (var i = 0; i < list.length; i++){
+
+            callback(list[i], i);
+        }
+    };
+
     return {
 
         getInput: function(){
@@ -299,14 +307,6 @@ var UIController = (function(){
            // Returns a Node List
           var fields = document.querySelectorAll(DOMstrings.expPercentageLabel);
 
-          var nodeListForEach = function(list, callback){
-
-              for (var i = 0; i < list.length; i++){
-
-                  callback(list[i], i);
-              }
-          };
-
           nodeListForEach(fields, function(current, index) {
 
               if (percentages[index] > 0) {
@@ -329,7 +329,24 @@ var UIController = (function(){
           month = now.getMonth();
           year = now.getFullYear();
 
-          document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' of ' + year;
+          document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
+
+        changedType: function(){
+
+          var fields = document.querySelectorAll(
+
+              DOMstrings.inputType + ',' +
+              DOMstrings.inputDesc + ',' +
+              DOMstrings.inputValue
+          );
+
+          nodeListForEach(fields, function(cur){
+
+              cur.classList.toggle('red-focus');
+          });
+
+          document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
         },
 
         // Expose DOMstrings globally so controller module can use them
@@ -359,6 +376,8 @@ var controller = (function(budgetCtrl, UICtrl){
         });
 
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
     };
 
     var updateBudget = function(){
